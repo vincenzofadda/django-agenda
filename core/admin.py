7744dcff246ec9270bcac37c3bf6720946c114f2
vincenzofadda <vincenzofadda@gmail.com>
2024-03-17 16:37:00 -0300

    Tabela de eventos criada

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Evento
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ('titulo',)
 
 # Register your models here.
+admin.site.register(Evento, EventoAdmin)
