ec18fdc19a10088626da9d452768cd5e1c8175f2
vincenzofadda <vincenzofadda@gmail.com>
2024-03-18 19:12:21 -0300

    add templates

--- core/models.py
+++ core/models.py
@@ -14,4 +14,7 @@ class Evento(models.Model):
     def __str__(self):
         return self.titulo
 
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M')
+
 # Create your models here.
